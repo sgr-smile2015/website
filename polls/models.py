6c098bad70054649a6e0e0c1fa8c77ff76f1f8bc
sgr <sgrqihai997@gmail.com>
2018-02-26 11:22:00 +0800

    models里添加meta选项

--- polls/models.py
+++ polls/models.py
@@ -8,10 +8,16 @@ class Question(models.Model):
     pub_date = models.DateField('Date Published')
 
     def was_published_recently(self):
-        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+        now = timezone.now()
+        return now - datetime.timedelta(days=1) <= self.pub_date <= now
 
     def __str__(self):
         return self.question_text
+
+    class Meta:
+        ordering = ["-pub_date"]
+        verbose_name = '发布问题'
+        verbose_name_plural = "发布问题"
 
 
 class Choice(models.Model):
--- polls/tests.py
+++ polls/tests.py
@@ -1,3 +1,12 @@
 from django.test import TestCase
+import datetime
+from .models import Question
+from django.utils import timezone
 
-# Create your tests here.
+
+class QuestionModelTest(TestCase):
+
+    def test_was_publish_with_future(self):
+        time = timezone.now() + datetime.timedelta(days=30)
+        future = Question(pub_date=time)
+        self.assertIs(future.was_published_recently(), False)
