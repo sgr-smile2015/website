540b7d8dbd7e86b503beca38a0fab154784030b5
sgr <sgrqihai997@gmail.com>
2018-03-02 10:26:15 +0800

    student视图内容添加,暂定为Student home page

--- website/urls.py
+++ website/urls.py
@@ -4,5 +4,8 @@ from django.contrib import admin
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^polls/', include('polls.urls')),
+
+    url(r'^', include('crm.urls')),
     url(r'^crm/', include('crm.urls')),
+    url(r'^stu/', include('student.urls')),
 ]
