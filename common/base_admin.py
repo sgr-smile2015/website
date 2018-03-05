fa5a27c40a13eee4eb0a1e283a5c083d067d2b11
sgr <sgrqihai997@gmail.com>
2018-03-05 22:40:33 +0800

    base_admin.py添加相关构造新字典的注释

--- common/base_admin.py
+++ common/base_admin.py
@@ -19,10 +19,17 @@ class CustomerFollowUpAdmin(BaseAdmin):
 
 
 def register(model_class, admin_class=None):
+    """
+    model_class: Customer class
+    admin_class: CustomerAdmin class
+    :return: 构造一个新字典 {"crm":{'customer':customer_admin}}
+    """
     if model_class._meta.app_label not in enabled_admins:
+        #enabled_admins['crm'] = {}
         enabled_admins[model_class._meta.app_label] = {}
 
     admin_class.model = model_class
+    #enabled_admins['crm']['customer'] = CustomerAdmin
     enabled_admins[model_class._meta.app_label][model_class._meta.model_name] = admin_class
 
 
