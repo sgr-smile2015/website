8d65627f9d86c9e670ea6d91e291071396fd2088
sgr <sgrqihai997@gmail.com>
2018-03-06 22:33:26 +0800

    添加分页过滤条件函数,utils.py->table_filter

---
+++ common/utils.py
@@ -0,0 +1,11 @@
+# coding: utf-8
+# author: sgr
+
+
+def table_filter(request, admin_class):
+    filter_conditions = {}
+
+    for k,v in request.GET.items():
+        filter_conditions[k] = v
+
+    return admin_class.model.objects.filter(**filter_conditions), filter_conditions
