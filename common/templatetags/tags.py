c35dabcb33a42fd2be68570a1c6585fef29bdc09
sgr <sgrqihai997@gmail.com>
2018-03-07 19:55:44 +0800

    添加render_filter_ele函数

--- common/templatetags/tags.py
+++ common/templatetags/tags.py
@@ -50,3 +50,28 @@ def render_page_ele(loop_counter, query_sets):
         ele = '''<li class="%s"><a href="?page=%s">%s</a></li>''' % (ele_class, loop_counter, loop_counter)
 
     return mark_safe(ele)
+
+
+@register.simple_tag
+def render_filter_ele(condtion, admin_class, filter_condtions):
+    select_ele = '''<select class="form-control" name='%s' ><option value=''>----</option>''' %condtion
+    field_obj = admin_class.model._meta.get_field(condtion)
+    if field_obj.choices:
+        selected = ''
+        for choice_item in field_obj.choices:
+            print("choice", choice_item, filter_condtions.get(condtion), type(filter_condtions.get(condtion)))
+            if filter_condtions.get(condtion) == str(choice_item[0]):
+                selected ="selected"
+
+            select_ele += '''<option value='%s' %s>%s</option>''' % (choice_item[0], selected, choice_item[1])
+            selected = ''
+
+    if type(field_obj).__name__ == "ForeignKey":
+        selected = ''
+        for choice_item in field_obj.get_choices()[1:]:
+            if filter_condtions.get(condtion) == str(choice_item[0]):
+                selected = "selected"
+            select_ele += '''<option value='%s' %s>%s</option>''' % (choice_item[0], selected, choice_item[1])
+            selected = ''
+    select_ele += "</select>"
+    return mark_safe(select_ele)
