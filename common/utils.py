e01f5a677fbe299b241ebd58579d15b9c4945eec
sgr <sgrqihai997@gmail.com>
2018-03-07 14:42:34 +0800

    部分html格式调整

--- common/utils.py
+++ common/utils.py
@@ -5,7 +5,7 @@
 def table_filter(request, admin_class):
     filter_conditions = {}
 
-    for k,v in request.GET.items():
+    for k, v in request.GET.items():
         filter_conditions[k] = v
 
     return admin_class.model.objects.filter(**filter_conditions), filter_conditions
