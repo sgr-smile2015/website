73f916d5c4d58dd45dcd0570aca8222598768adb
sgr <sgrqihai997@gmail.com>
2018-03-15 11:29:16 +0800

    try-django-1.11 web site

--- mysite/urls.py
+++ mysite/urls.py
@@ -3,5 +3,7 @@ from . import views
 
 
 urlpatterns = [
-    url(r'^$', views.index, name="mysite"),
+    url(r'^$', views.index, name="index"),
+    url(r'^about$', views.about, name="about"),
+    url(r'^contact$', views.contact, name="contact"),
 ]
--- mysite/views.py
+++ mysite/views.py
@@ -5,13 +5,21 @@ from random import randint
 
 def index(request):
     html_var = 'f string'
-    num = randint(0,100000)
-    some_list = [num, randint(0,100000), randint(0,100000)]
+    num = randint(0, 100000)
+    some_list = [num, randint(0, 100000), randint(0, 100000)]
     context = {
         'boll_var': True,
+        'html_var': html_var,
         'num': num,
         'some_lists': some_list
     }
 
-    return render(request, 'page.html', context)
+    return render(request, 'mysite/home.html', context)
 
+
+def about(request):
+    return render(request, 'mysite/about.html')
+
+
+def contact(request):
+    return render(request, 'mysite/contact.html')
