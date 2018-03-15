8ff7cbdddeddb43e48f28914f1b56a0fbf950d38
sgr <sgrqihai997@gmail.com>
2018-03-15 15:51:12 +0800

    use templateview base class

--- mysite/urls.py
+++ mysite/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 
 urlpatterns = [
-    url(r'^$', views.index, name="index"),
+    url(r'^$', views.HomeView.as_view(), name="index"),
     url(r'^about$', views.about, name="about"),
     url(r'^contact$', views.contact, name="contact"),
 ]
--- mysite/views.py
+++ mysite/views.py
@@ -2,19 +2,7 @@ from django.shortcuts import render
 from random import randint
 #from django.http import HttpResponse
 
-
-def index(request):
-    html_var = 'f string'
-    num = randint(0, 100000)
-    some_list = [num, randint(0, 100000), randint(0, 100000)]
-    context = {
-        'boll_var': True,
-        'html_var': html_var,
-        'num': num,
-        'some_lists': some_list
-    }
-
-    return render(request, 'mysite/home.html', context)
+from django.views.generic import TemplateView
 
 
 def about(request):
@@ -23,3 +11,23 @@ def about(request):
 
 def contact(request):
     return render(request, 'mysite/contact.html')
+
+
+class HomeView(TemplateView):
+
+    template_name = 'mysite/home.html'
+    #return super(HomeView, self).get_context_data(**kwargs)
+
+    def get_context_data(self, **kwargs):
+        html_var = 'git string'
+        num = randint(0, 100000)
+        some_list = [randint(0, 100000),
+                     randint(0, 100000),
+                     randint(0, 100000)]
+        context = {
+            'boll_var': True,
+            'html_var': html_var,
+            'num': num,
+            'some_lists': some_list
+        }
+        return context
