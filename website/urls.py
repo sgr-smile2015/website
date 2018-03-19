9951d8dec6de2ff230de96d9edbd180f2887d1a2
sgr <sgrqihai997@gmail.com>
2018-03-19 17:05:35 +0800

    rename mysite-> restaurants

--- website/urls.py
+++ website/urls.py
@@ -4,7 +4,7 @@ from django.contrib import admin
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
 
-    url(r'^', include('mysite.urls')),
+    url(r'^', include('restaurants.urls')),
     url(r'^crm/', include('crm.urls')),
     url(r'^stu/', include('student.urls')),
     url(r'^comm/', include('common.urls')),
